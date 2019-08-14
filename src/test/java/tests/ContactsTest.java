a0a9f4b0208153d307293270a94ff111fb770957
Fatma Ince <fatmaturkan13@gmail.com>
2019-08-14 08:49:36 -0500

    adding changes

--- src/test/java/tests/ContactsTest.java
+++ src/test/java/tests/ContactsTest.java
@@ -8,6 +8,9 @@ public class ContactsTest {
     public void test1(){
         System.out.println("Driver open vytrack");
         System.out.println("Driver goes to contacts page");
+        System.out.println("learning creating git branch");
+        git status
+
 
     }
 }
